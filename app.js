c449ad54d57fe12958c8b685d27044db8027e1ae
Wenqer <wenqeer@gmail.com>
2014-01-16 13:06:56 +0200

    refactoring

--- app.js
+++ app.js
@@ -1,30 +1,47 @@
 var http    = require('http');
 var Cookies = require('cookies');
-var fs =      require('fs');
-var ua =      require('universal-analytics');
-var img = fs.readFileSync('./dg-tilelayer-base-copyright.png');
+var fs      = require('fs');
+var qs      = require('querystring');
+var config  = require('./config.json');
+var request = require('request');
+var img = fs.readFileSync('./' + config.image);
+var cookie = 'cid_' + config.projectPreffix;
 
-server = http.createServer( function(req, res) {
+server = http.createServer(function (req, res) {
   var cookies = new Cookies(req, res),
-      clientid,
-      referal = req.headers['referer'] || '';
+      referal = req.headers['referer'] || '',
+      params = {
+        dp: '/',
+        dh: referal,
+        v: config.apiVersion,
+        tid: config.UA,
+        t: 'pageview'
+      },
+      clientid;
 
-  if ( req.url == '/dg-tilelayer-base-copyright.png' ) {
-    clientid = cookies.get('clientid');
+  if (req.url == '/' + config.image) {
+
+    clientid = cookies.get(cookie);
+
     if (!clientid) {
       clientid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
           var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
           return v.toString(16);
       });
-      cookies.set('clientid', clientid, {httpOnly: false});
-    }
-    var visitor = ua('UA-13117927-2', clientid);
 
-    visitor.pageview('/', referal).send();
+      cookies.set(cookie, clientid, {httpOnly: false});
+    }
+
+    params.cid = clientid;
+
+    var path = config.hostname + config.path + '?' + qs.stringify(params);
+
+    request.post(path);
 
     res.writeHead(200, {'Content-Type': 'image/png' });
     res.end(img, 'binary');
   }
+
 }).listen(8888);
 
 console.log('Server has started.');
