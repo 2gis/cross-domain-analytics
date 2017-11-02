7d8f52281817537a21babc9236f2623090fb2811
Mstislav Zhivodkov <stevemyz@gmail.com>
2017-11-02 15:47:43 +0700

    Change production path log

--- logger.js
+++ logger.js
@@ -1,8 +1,10 @@
 var winston = require('winston');
 
+var logPath = process.env.NODE_ENV === 'development' ? 'app.log' : '/var/log/cross-domain-analytics.log';
+
 module.exports = new (winston.Logger)({
     transports: [
         new (winston.transports.Console)(),
-        new (winston.transports.File)({ filename: 'app.log' })
+        new (winston.transports.File)({ filename: logPath })
     ]
 });
